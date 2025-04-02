be51036e6fd795a32c7081e09bcfd524c5b5d486
Trung3Tech <75372157+Trung3Tech@users.noreply.github.com>
2025-04-02 16:26:20 +0700

    add, update product

--- ShopMgmt/src/main/java/com/mytech/shopmgmt/models/Product.java
+++ ShopMgmt/src/main/java/com/mytech/shopmgmt/models/Product.java
@@ -1,9 +1,25 @@
 package com.mytech.shopmgmt.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity(name = "Product")
+@Table(name = "Products")
+@NamedQuery(name = "Product.findByCode", query = "SELECT p FROM Product p WHERE p.code = :code")
+@NamedQuery(name = "Product.findByName", query = "SELECT p FROM Product p WHERE p.name like :name")
+@NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p")
+//@NamedQuery(name = "Product.findall", query = "SELECT e FROM Ten Entity alias e")
 public class Product {
 
+	@Id
 	private String code;
+	
+	@Column(name = "name", nullable = false, length = 256, unique = true)
 	private String name;
+	
 	private double price;
 	private String imagePath;
 
